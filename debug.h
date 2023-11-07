1b472f64b23de3b323c85d10f40d5be5ccf97e6e
wordandahalf <10679776+wordandahalf@users.noreply.github.com>
2023-11-07 17:13:50 -0500

    Debug prints are implementation-agnostic

--- debug.h
+++ debug.h
@@ -8,22 +8,22 @@
 #include <stdio.h>
 #include "processor.h"
 
-void print_registers(cpu_state *state) {
+void print_registers(int *register_file) {
     for (int i = 0; i < 8; i++) {
-        printf("R%-2d: %-10d ", i, state->register_file[i]);
+        printf("R%-2d: %-10d ", i, register_file[i]);
     }
     printf("\n");
     for (int i = 0; i < 8; i++) {
-        printf("R%-2d: %-10d ", i + 8, state->register_file[i + 8]);
+        printf("R%-2d: %-10d ", i + 8, register_file[i + 8]);
     }
     printf("\n");
 }
 
-void print_memory(cpu_state *state) {
-    for (int i = 0; i < MAX_WORDS_OF_DATA; i += 10) {
+void print_memory(int *data_memory) {
+    for (int i = 0; i < MAX_WORDS_OF_DATA; i += 20) {
         printf("%4d ", i);
         for (int j = 0; j < 20; j++) {
-            printf("%-4d ", state->data_memory[j]);
+            printf("%-4d ", data_memory[i + j]);
         }
         printf("\n");
     }
